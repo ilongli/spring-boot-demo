4923fa8f1f3e02db4979ed1e54ac5dc544b18473
ilongli <351365415@qq.com>
2020-01-26 22:01:54 +0800

    add files

---
+++ src/test/java/com/ilongli/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilongli.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
